849a614e1319dedbc5a849a5af645fa7bde09860
victor666 <1960892068@qq.com>
2019-12-19 14:46:06 +0800

    完善 java 业务框架

--- src/main/java/com/ymx/recommendation/model/dao/LuckyMoneyDao.java
+++ src/main/java/com/ymx/recommendation/model/dao/LuckyMoneyDao.java
@@ -2,6 +2,8 @@ package com.ymx.recommendation.model.dao;
 
 import com.ymx.recommendation.model.entity.Luckymoney;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LuckyMoneyDao extends JpaRepository<Luckymoney, Integer> {
 }
